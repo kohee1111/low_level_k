3a2591c2d49118d832e3b01471187838a1eecf78
kohee <koheetg@gmail.com>
2026-02-27 13:59:42 +0000

    Create all_of_hex_desimal.s

---
+++ all_of_hex_desimal.s
@@ -0,0 +1,42 @@
+Learning the riscv programming language the main target is to learn all the patterns of hexdesimal system insted of memorizing shit 
+hexdesimal is 16 base so i have to memorize some of the * and / of 16 
+first task is i have to memorize all the * of 16 ; 
+16 * 1 = 16 ; 
+16 * 2 = 32 ; 
+16 * 3 = 48 ; 
+16 * 4 = 64 ; 
+16 * 5 = 80 ; 
+16 * 6 = 96 ; 
+16 * 7 = 112 ; 
+16 * 8 = 128 ; 
+16 * 9 = 144  ; 
+16 * 10 = 160 ; 
+....and also some powers like 
+every hex digit is 4 bits 
+digit 1 = 16 ^ 0 = 1 
+digit 2 = 16 ^ 2 = 256
+digit 3 = 16 ^ 3 = 4096 
+digit 4 = 16 ^ 4 = 65536 
+...and also here is the basic hexadesimal numbers 
+like :
+1 = 0x1 
+2 = 0x2 
+3 = 0x3 
+4 = 0x4
+5 = 0x5
+6 = 0x6 
+7 = 0x7
+8 = 0x8 
+9 = 0x9 
+10 = A 
+11 = B
+12 = C 
+13 = D 
+14 = E
+15 = F 
+16 = 0x10 
+17 = 0x11 
+18 = 0x12 
+19 = 0x13
+20 = 0x14
+
