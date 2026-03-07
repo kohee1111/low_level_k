52a5acb0257478e41b8fbd7d0d7b7bc4115c9db5
kohee <koheetg@gmail.com>
2026-03-07 08:49:17 +0000

    Update links.s

--- links.s
+++ links.s
@@ -1,3 +1,4 @@
 most recommanded low level tutorial of my life about c and asm : https://cass-kul.github.io/ 
 to learn and research about ai prompt jail breaking ---> https://www.hackaprompt.com/
 this website to begin anything ---> https://exercism.org/dashboard
+for all kind of docs----->https://devdocs.io/
