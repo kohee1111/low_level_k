04b891fcdef516f4cc6617d7b7f8c06c6b65c4dc
kohee <koheetg@gmail.com>
2026-03-11 06:10:55 +0000

    Update links.s

--- links.s
+++ links.s
@@ -3,3 +3,4 @@ to learn and research about ai prompt jail breaking ---> https://www.hackaprompt
 this website to begin anything ---> https://exercism.org/dashboard
 for all kind of docs----->https://devdocs.io/
 another ---->https://learnxinyminutes.com/
+before building an os -> https://wiki.osdev.org/Expanded_Main_Page
